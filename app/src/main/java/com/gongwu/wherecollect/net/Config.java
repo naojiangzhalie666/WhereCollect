f713d2f0881dc3360f59ce993b1284f85e3c6753
mucll <351371167@qq.com>
2021-04-19 21:54:33 +0800

    4.3 修改版本号

--- app/src/main/java/com/gongwu/wherecollect/net/Config.java
+++ app/src/main/java/com/gongwu/wherecollect/net/Config.java
@@ -29,6 +29,6 @@ public class Config {
     public static final String WEB_GUIDER_NAME = "帮助与引导";
     public static final String WEB_GUIDER_URL = IS_TEST ? "http://std.shouner.com/popularize/" : "https://rg.shouner.com/";
     //个人详情 显示小版本,发一个测试版本就加一,好让测试区别 测试版本
-    public static final String VERSION = "71";
+    public static final String VERSION = "72";
 
 }
